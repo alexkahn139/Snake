633aca4f8a9d32e148b06c108b4defa30c0f34ec
alexkahn139 <alexandrekahn139@gmail.com>
2016-12-11 21:00:43 +0100

    Initial Commit

---
+++ Snake/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Snake
+//
+//  Created by Alexandre Kahn on 11/12/2016.
+//  Copyright © 2016 Alexandre Kahn. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
