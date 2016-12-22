dcffb31edaaa554b04574967c2b62a463854f9b5
alexkahn139 <alexandrekahn139@gmail.com>
2016-12-22 12:03:19 +0100

    Added SDL-TTF.framework

---
+++ Snake/input_output.c
@@ -0,0 +1,9 @@
+//
+//  input_output.c
+//  Snake
+//
+//  Created by Alexandre Kahn on 22/12/2016.
+//  Copyright © 2016 Alexandre Kahn. All rights reserved.
+//
+
+#include "input_output.h"
---
+++ Snake/input_output.h
@@ -0,0 +1,14 @@
+//
+//  input_output.h
+//  Snake
+//
+//  Created by Alexandre Kahn on 22/12/2016.
+//  Copyright © 2016 Alexandre Kahn. All rights reserved.
+//
+
+#ifndef input_output_h
+#define input_output_h
+
+#include <stdio.h>
+
+#endif /* input_output_h */
